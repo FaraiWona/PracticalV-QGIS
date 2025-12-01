72ffa2ce722375acc73febbd3f6c908a79e812c9
Bsc-04-21 <Bsc-04-21@unima.ac.mw>
2025-12-01 18:23:59 +0200

    initialising backend and creating database connection

--- webgis/app.py
+++ webgis/app.py
@@ -1,3 +1,13 @@
 from flask import Flask, jsonify, render_template, request
 import psycopg2
 import math
+
+app = Flask(__name__)
+
+conn_params = dict(
+    dbname="Hospita_GIS",
+    user="postgres",
+    password="Pedri123",
+    host="localhost",
+    port="5432"
+)

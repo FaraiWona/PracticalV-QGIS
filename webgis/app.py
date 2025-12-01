6eb45c815e5bbc71f5f2c82a1bd04af06d96c676
Bsc-04-21 <Bsc-04-21@unima.ac.mw>
2025-12-01 18:28:15 +0200

    latitude math code

--- webgis/app.py
+++ webgis/app.py
@@ -16,3 +16,12 @@ ef connect_db():
     return psycopg2.connect(**conn_params)
 
 def distance_km(lat1, lon1, lat2, lon2):
+
+     R = 6371
+    dlat = math.radians(lat2 - lat1)
+    dlon = math.radians(lon2 - lon1)
+    a = (math.sin(dlat/2)**2 +
+         math.cos(math.radians(lat1)) *
+         math.cos(math.radians(lat2)) *
+         math.sin(dlon/2)**2)
+    return round(R * (2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))), 2)
